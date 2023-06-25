fb83a9dcefb179518a4823b0d24fd050310a111e
timueh <t.muehlpfordt@mailbox.org>
2023-06-25 18:12:53 +0200

    add readme

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-
 app ?= fib_app
 
 build:
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "ec2_public_access" {
-  value = aws_instance.api.public_dns
-}
-
 output "address" {
   value = "curl http://${aws_instance.api.public_dns}:${var.port}/fibonacci/:n (replace :n by a number)"
 }
